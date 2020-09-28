10778e2cb266ecc87cc20172f2ac9dc2cdec38b0
Lingchen Zhu <lzhu14@slb.com>
2020-09-28 00:04:02 -0400

    update myapp.py

--- myapp.py
+++ myapp.py
@@ -1,5 +1,5 @@
 def app(environ, start_response):
-	data = b"Hello, World!\nThis is a Gunicorn server!"
+    data = b"Hello, World!\nFrom Gunicorn\n"
     start_response("200 OK", [
         ("Content-Type", "text/plain"),
         ("Content-Length", str(len(data)))
