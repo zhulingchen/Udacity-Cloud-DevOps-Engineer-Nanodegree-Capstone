e4824feb1bd664a3426724fb39713b8f5d9ede4c
Lingchen Zhu <lzhu14@slb.com>
2020-10-02 22:26:41 -0400

    run gunicorn at container launch

--- myapp/myapp.py
+++ myapp/myapp.py
@@ -1,3 +1,6 @@
+# reference:
+# https://www.digitalocean.com/community/tutorials/how-to-serve-flask-applications-with-gunicorn-and-nginx-on-ubuntu-18-04
+
 from flask import Flask
 app = Flask(__name__)
 
--- myapp/wsgi.py
+++ myapp/wsgi.py
@@ -1,3 +1,6 @@
+# reference:
+# https://www.digitalocean.com/community/tutorials/how-to-serve-flask-applications-with-gunicorn-and-nginx-on-ubuntu-18-04
+
 from myapp import app
 
 if __name__ == "__main__":
